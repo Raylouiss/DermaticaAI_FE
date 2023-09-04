c2999e39874499486768fc9ed789a3e8700a6635
bewe <16521486@mahasiswa.itb.ac.id>
2023-09-04 11:55:43 +0700

    style: history

--- lib/screen/history.dart
+++ lib/screen/history.dart
@@ -12,105 +12,95 @@ class _HistoryState extends State<History> {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF5F93A0),
-        title: Text(
-          'History',
-          textAlign: TextAlign.center,
-        ),
-        centerTitle: true,
-        leading: IconButton(
-          icon: Row(
-            children: [
-              Icon(Icons.arrow_back, color: Colors.black,),
-              Text('Back',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold
-                ),
-              ),
-            ],
+    return WillPopScope(
+        onWillPop: () async {
+          return false;
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xFF5F93A0),
+            title: const Text(
+              'History',
+              textAlign: TextAlign.center,
+            ),
+            centerTitle: true,
+            automaticallyImplyLeading: false,
           ),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        leadingWidth: 90,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: 10, // Number of items
-              itemBuilder: (context, index) {
-                return Container(
-                  padding: EdgeInsets.all(12),
-                  width: width,
-                  margin: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(15.0),
-                            child: Image.asset('assets/logo_kelompok.png'),
-                          ),
-                          SizedBox(width: 10),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  Image.asset('assets/res_danger_icon.png'),
-                                  SizedBox(width: 10),
-                                  Text(
-                                    'Acne Vulgaris',
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(height: 10,),
-                              Align(
-                                alignment: Alignment.centerLeft,
-                                child: Text('7 July 2022'),
-                              ),
-                            ],
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: 10, // Number of items
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: const EdgeInsets.all(12),
+                      width: width,
+                      margin: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
                           ),
                         ],
                       ),
-                      IconButton(
-                        icon: Icon(Icons.chevron_right),
-                        onPressed: () {
-                          // Add action here
-                        },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(15.0),
+                                child: Image.asset('assets/logo_kelompok.png'),
+                              ),
+                              const SizedBox(width: 10),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Image.asset('assets/res_danger_icon.png'),
+                                      const SizedBox(width: 10),
+                                      const Text(
+                                        'Acne Vulgaris',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  const Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text('7 July 2022'),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.chevron_right),
+                            onPressed: () {
+                              // Add action here
+                            },
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                );
-              },
+                    );
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
